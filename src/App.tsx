32a5d1388c021bc5105f073b9ee1ea0f6560c321
jarif <xjarifx@gmail.com>
2026-03-03 11:49:04 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import layer0 from "./assets/layer_0.png";
+import layer1 from "./assets/layer_1.png";
+import layer2 from "./assets/layer_2.png";
+import layer3 from "./assets/layer_3.png";
+import layer4 from "./assets/layer_4.png";
+import layer5 from "./assets/layer_5.png";
+import layer6 from "./assets/layer_6.png";
+
+export default function App() {
+  return (
+    <div className="parallax">
+      <div className="parallax__layer parallax__layer__0">
+        <img src={layer0} alt="" aria-hidden="true" />
+      </div>
+      <div className="parallax__layer parallax__layer__1">
+        <img src={layer1} alt="" aria-hidden="true" />
+      </div>
+      <div className="parallax__layer parallax__layer__2">
+        <img src={layer2} alt="" aria-hidden="true" />
+      </div>
+      <div className="parallax__layer parallax__layer__3">
+        <img src={layer3} alt="" aria-hidden="true" />
+      </div>
+      <div className="parallax__layer parallax__layer__4">
+        <img src={layer4} alt="" aria-hidden="true" />
+      </div>
+      <div className="parallax__layer parallax__layer__5">
+        <img src={layer5} alt="" aria-hidden="true" />
+      </div>
+      <div className="parallax__layer parallax__layer__6">
+        <img src={layer6} alt="" aria-hidden="true" />
+      </div>
+      <div className="parallax__cover" />
+    </div>
+  );
+}
